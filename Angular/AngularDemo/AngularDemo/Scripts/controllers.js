2c3b6a40ad9079d7e3f933f9dc24f1f9174ef163
James Bender <james@jamescbender.com>
2015-10-11 16:44:46 -0400

    adding content

---
+++ Angular/AngularDemo/AngularDemo/Scripts/controllers.js
@@ -0,0 +1,31 @@
+﻿var servicesExampleApp = angular.module('servicesExampleApp', ['ngResource']);
+
+//This syntax to declare this to deal with minification is:
+// <app>.controllercontroller('UserRegistrationController', ['$scope', 'Users', function ($scope, Users) {...}]
+servicesExampleApp.controller('UserRegistrationController', function ($scope, Users) {
+    Users.query(function (data) {
+        $scope.users = data;
+    });
+
+    $scope.getUser = function (id) {
+        Users.get({ id: id }, function (data) {
+            $scope.currentUser = data;
+        });
+    };
+
+    $scope.addUser = function () {
+        Users.save($scope.currentUser, function () {
+            Users.query(function (data) {
+                $scope.users = data;
+            });
+            $scope.currentUser = {};
+        });
+    }
+});
+
+//This equivlent syntax to declare this that doesn't deal with minification is:
+// <app>.controllercontroller('User', function ($scope, Users) {...})
+servicesExampleApp.factory('Users', ['$resource', '$scope', function ($resource, $scope) {
+        return $resource("/api/Users/:id");
+    }
+]);
